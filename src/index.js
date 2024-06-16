48dc01c27ce09182150fa32ef2ce9cd2571aa5fa
Dan Retegan <danretegan@yahoo.com>
2024-06-16 13:05:44 +0300

    Real-time Listeners
    .onSnapshot()
    .docChanges()

--- src/index.js
+++ src/index.js
@@ -16,21 +16,31 @@ const addRecipe = (recipe, id) => {
   list.innerHTML += html;
 };
 
-//! get documents:
-db.collection('recipes')
-  .get()
-  .then(snapshoot => {
-    // when we have the data
-    // console.log(snapshoot.docs[0].data());
-    snapshoot.docs.forEach(doc => {
-      //   console.log(doc.data());
-      //   console.log(doc.id);
-      addRecipe(doc.data(), doc.id);
-    });
-  })
-  .catch(err => {
-    console.log(err);
+//! functie de stergere reteta din UI:
+const deleteRecipe = id => {
+  const retete = document.querySelectorAll('li');
+  retete.forEach(elem => {
+    if (elem.getAttribute('recipe-id') === id) {
+      elem.remove();
+    }
   });
+};
+
+//! get documents:
+// TODO Real-time Listeners [.onSnapshot()]
+db.collection('recipes').onSnapshot(snapshot => {
+  //   console.log(snapshot.docChanges());
+  snapshot.docChanges().forEach(change => {
+    // console.log(change);
+    const doc = change.doc;
+    // console.log(doc);
+    if (change.type === 'added') {
+      addRecipe(doc.data(), doc.id);
+    } else if (change.type === 'removed') {
+      deleteRecipe(doc.id);
+    }
+  });
+});
 
 //! add documents:
 form.addEventListener('submit', e => {
